fdeffd567d97579e03e271773c0e7c49225d1b1b
Sean McKenna <mckennapsean@gmail.com>
2025-10-20 21:26:08 -0600

    fixup! get svelte kit running app with command npm run dev...

---
+++ svelte.config.js
@@ -0,0 +1,19 @@
+// svelte.config.js
+import adapter from '@sveltejs/adapter-auto';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
+	// for more information about preprocessors
+	preprocess: vitePreprocess(),
+
+	kit: {
+		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
+		// If your environment is not supported or you prefer to specify an adapter,
+		// install one and add it here.
+		adapter: adapter()
+	}
+};
+
+export default config;
